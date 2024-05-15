c09734ee79819262b2a71ca07d86146c7edb8ab2
Matheus Saturnino <80081045+MthSat@users.noreply.github.com>
2024-05-14 20:44:23 -0400

    mudando o texto de acerto 2

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ function verificarChute() {
     let chute = document.querySelector('input').value;
     
     if (chute == numeroSecreto) {
-        exibirTextoNaTela('h1', 'Acertou o miserável!');
+        exibirTextoNaTela('h1', 'Acertooou o miserável!');
         let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
         let mensagemTentativas = `Você descobriu o número secreto com ${tentativas} ${palavraTentativa}!`;
         exibirTextoNaTela('p', mensagemTentativas);
